099dcdfdb7df5a7add780991fc5fe2c609732b7f
Nick YOOO <outgrowthr@gmail.com>
2023-08-08 09:55:40 +0900

    #1 Chore: 수빈님 코드 반영

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  if (
-    window.location.pathname !== '/signup' &&
-    window.location.pathname !== '/login' &&
-    window.location.pathname !== '/' &&
-    window.location.pathname !== '/post' &&
-    window.location.pathname !== '/board' &&
-    window.location.pathname !== '/detail/:id' &&
-    window.location.pathname !== '/user/:id' &&
-    window.location.pathname !== '/report'
-  ) {
-    return null;
-  }
+  if (!['/signup', '/login', '/', '/post', '/board', '/detail/:id', '/user/:id', '/report'].includes(window.location.pathname)) return null;
 
   return <div>Footer</div>;
 };
