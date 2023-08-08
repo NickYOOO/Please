1d481902b3dd42972d9ff75f96c15e348be1b1d2
subin <fnqhdtm@gmail.com>
2023-08-08 17:28:17 +0900

    Design: 홈페이지 gif 추가

--- src/components/footer/Footer.styles.ts
+++ src/components/footer/Footer.styles.ts
@@ -1,1 +1,31 @@
 import { styled } from 'styled-components';
+
+export const FooterLayout = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  /* height: 213; */
+  padding-top: 20px;
+  background: #ffefab;
+
+  & > p {
+    line-height: 2;
+    /* color: #ff6f2c; */
+    /* color: #ff004c; */
+    color: #444444;
+    /* color: #0074dd; */
+    font-size: 13px;
+  }
+`;
+
+export const FooterBox = styled.div`
+  display: inline-flex;
+  margin: 10px 0;
+  gap: 20px;
+
+  & > a {
+    position: relative;
+    cursor: pointer;
+  }
+`;
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import { FaFigma, FaGithub } from 'react-icons/fa';
+import { SiNotion } from 'react-icons/si';
+import * as Styled from './Footer.styles';
 
 const Footer = () => {
   if (!['/signup', '/login', '/', '/post', '/board', '/detail/:id', '/user/:id', '/report'].includes(window.location.pathname)) return null;
 
-  return <div>Footer</div>;
+  return (
+    <Styled.FooterLayout>
+      <p>SpartaCodingClub</p>
+      <p>유지완, 임선우, 최윤서, 임수빈, 박기태</p>
+      <p> &copy; 2023 이야~ All rights reserved</p>
+
+      <Styled.FooterBox>
+        <a href="https://github.com/NickYOOO/Please">
+          <FaGithub size="25" color="#444444" />
+        </a>
+        <a href="https://www.figma.com/file/rTVLj1VOeiFSdYN4N9Qpct/%EB%B6%80%ED%83%81%ED%95%B4?type=design&node-id=0-1&mode=design&t=4CFhikaX2p6gtRzv-0https://www.figma.com/file/rsgKzR7L6PkEChGLXin56x/5%EC%A1%B0%3A-%EB%8B%B9%EC%8B%A0%EC%9D%98-%EB%AC%B8%ED%99%94%EC%9C%A0%EC%82%B0-%EB%8B%B5%EC%82%AC%EA%B8%B0---Design?type=design&node-id=126%3A153&mode=design&t=ESyyUcq3jTfhx52s-1">
+          <FaFigma size="25" color="#444444" />
+        </a>
+        <a href="https://teamsparta.notion.site/2-fd5ba9c720454186bda02ce887e68461">
+          <SiNotion size="25" color="#444444" />
+        </a>
+      </Styled.FooterBox>
+    </Styled.FooterLayout>
+  );
 };
 
 export default Footer;
