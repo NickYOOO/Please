a625442b6a2de68b7b9b34799db7919a1f5e3511
Sunwoo <dlatjsdn205@Naver.com>
2023-08-08 16:35:44 +0900

    Feat: Layout Component

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { useRef } from "react";
+import { styled } from "styled-components";
+
+const CategoryLi = styled.li`
+  
+`
 
 const PostPage = () => {
-  return <div>PostPage</div>;
+  const BoxRef = useRef(null);
+  const categorys: string[] = ["배달", "청소", "조립", "역할대행", "동행,돌봄", "반려동물", "벌레퇴치", "기타"]
+  return (
+    <>
+      <ul ref={BoxRef}>
+        {categorys.map((item, i) => <li key="i">{item}</li> )}
+      </ul>
+    </>
+  );
 };
 
 export default PostPage;
