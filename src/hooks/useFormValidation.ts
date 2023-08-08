992bfa65ef3f0f350491634d364973d735c147cb
Choi Yoonseo <choiys1103@naver.com>
2023-08-08 19:33:15 +0900

    feat: 회원가입 (json-server-auth 사용)

---
+++ src/hooks/useFormValidation.ts
@@ -0,0 +1,159 @@
+import { useState } from 'react';
+
+const useFormValidation = () => {
+  const [formState, setFormState] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [validationMsg, setValidationMsg] = useState({
+    usernameMsg: '',
+    emailMsg: '',
+    passwordMsg: '',
+    confirmPasswordMsg: '',
+  });
+
+  const [validationState, setValidationState] = useState({
+    usernameState: false,
+    emailState: false,
+    passwordState: false,
+    confirmPasswordState: false,
+  });
+
+  const handleJoinInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormState(prev => ({ ...prev, [name]: value }));
+
+    switch (name) {
+      case 'username':
+        {
+          let msg = '';
+          let currentState = false;
+          if (!validateValue(name, value)) {
+            msg = '사용 가능한 닉네임이 아닙니다.(특수문자 제외 2~10자)';
+            currentState = false;
+          } else {
+            msg = '사용 가능한 닉네임 형식입니다.';
+            currentState = true;
+          }
+          setValidationMsg(prev => ({
+            ...prev,
+            usernameMsg: msg,
+          }));
+
+          setValidationState(prev => ({
+            ...prev,
+            usernameState: currentState,
+          }));
+        }
+        break;
+      case 'email':
+        {
+          let msg = '';
+          let currentState = false;
+
+          if (!validateValue(name, value)) {
+            msg = '이메일 형식이 올바르지 않습니다.';
+            currentState = false;
+          } else {
+            msg = '사용 가능한 이메일 형식입니다.';
+            currentState = true;
+          }
+          setValidationMsg(prev => ({
+            ...prev,
+            emailMsg: msg,
+          }));
+          setValidationState(prev => ({
+            ...prev,
+            emailState: currentState,
+          }));
+        }
+        break;
+      case 'password':
+        {
+          let msg = '';
+          let currentState = false;
+          if (!validateValue(name, value)) {
+            msg = '영문, 숫자, 특수문자 조합 8~15 자리로 입력해주세요.';
+            currentState = false;
+          } else {
+            msg = '사용 가능한 비밀번호입니다.';
+            currentState = true;
+          }
+          setValidationMsg(prev => ({
+            ...prev,
+            passwordMsg: msg,
+          }));
+          setValidationState(prev => ({
+            ...prev,
+            passwordState: currentState,
+          }));
+        }
+        break;
+      case 'confirmPassword':
+        {
+          let msg = '';
+          let currentState = false;
+          if (formState.password !== value) {
+            msg = '비밀번호가 일치하지 않습니다.';
+            currentState = false;
+          } else {
+            msg = '비밀번호가 일치합니다.';
+            currentState = true;
+          }
+          setValidationMsg(prev => ({
+            ...prev,
+            confirmPasswordMsg: msg,
+          }));
+          setValidationState(prev => ({
+            ...prev,
+            confirmPasswordState: currentState,
+          }));
+
+          const isFormValid = validationState.usernameState && validationState.emailState && validationState.passwordState && currentState; // 비밀번호 확인 입력란의 상태
+
+          setValidationState(prev => ({
+            ...prev,
+            isFormValid: isFormValid,
+          }));
+        }
+        break;
+      default:
+        return;
+    }
+    if (value.trim() === '') {
+      setValidationMsg(prev => ({
+        ...prev,
+        [`${name}Msg`]: '',
+      }));
+      setValidationState(prev => ({
+        ...prev,
+        [`${name}State`]: false,
+      }));
+    }
+  };
+
+  const validateValue = (name: string, value: string) => {
+    switch (name) {
+      case 'username':
+        return /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{2,10}$/.test(value);
+      case 'email':
+        return /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(value);
+      case 'password':
+        return /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[~?!@#$%^&*_-]).{8,15}$/.test(value);
+      default:
+        return false;
+    }
+  };
+
+  return {
+    formState,
+    validationMsg,
+    validationState,
+    handleJoinInputChange,
+  };
+};
+
+export default useFormValidation;
