1ddc19aef3e4983641b5e5d2a7885a648b53a4a1
Nick YOOO <outgrowthr@gmail.com>
2023-08-08 13:01:20 +0900

    Design: footer styled components 생성 및 분리

--- src/components/footer/Footer.styles.ts
+++ src/components/footer/Footer.styles.ts
@@ -1,1 +1,28 @@
 import { styled } from 'styled-components';
+
+export const FooterLayout = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 213;
+  padding-top: 50px;
+  background: #f8f0e5;
+
+  & > p {
+    line-height: 2;
+    color: #ff6f2c;
+    font-size: 16px;
+  }
+`;
+
+export const FooterBox = styled.div`
+  display: inline-flex;
+  margin-top: 20px;
+  gap: 20px;
+
+  & > a {
+    position: relative;
+    cursor: pointer;
+  }
+`;
