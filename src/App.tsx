21fc18df15729e52846d970094c0299813ec9abf
Sunwoo <dlatjsdn205@Naver.com>
2023-08-08 09:56:03 +0900

    Chore: Test Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Router from './shared/Router';
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+  < Router/>
+  );
+  }
 
 export default App;
