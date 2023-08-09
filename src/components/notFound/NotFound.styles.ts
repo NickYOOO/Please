a7508c527f9d243053a77afe520e5f5a5afc0d95
Nick YOOO <outgrowthr@gmail.com>
2023-08-09 15:58:56 +0900

    Chore: 안쓰는 이미지 정리

---
+++ src/components/notFound/NotFound.styles.ts
@@ -0,0 +1,25 @@
+import { styled } from 'styled-components';
+
+export const FullPageBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #fff;
+  padding: 10px;
+
+  & > h1 {
+    font-size: 60px;
+    color: #000000;
+    margin: 40px 0 40px 0;
+    word-break: keep-all;
+  }
+
+  & > p {
+    font-size: 34px;
+    font-weight: 500;
+    color: #000000;
+    word-break: keep-all;
+  }
+`;
