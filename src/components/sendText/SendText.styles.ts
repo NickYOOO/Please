70d2f5763b75e460471faa2babcd47e425be944a
Choi Yoonseo <choiys1103@naver.com>
2023-08-11 17:38:18 +0900

    design: 쪽지 보내기 ui 수정

--- src/components/sendText/SendText.styles.ts
+++ src/components/sendText/SendText.styles.ts
@@ -1,18 +1,44 @@
 import { styled } from 'styled-components';
 
 export const TextParagraph = styled.p`
+  position: absolute;
+  z-index: 1;
+  top: 50px;
+  left: 0;
+  width: 100%;
   text-align: center;
-  font-size: 18px;
+  font-size: 20px;
   font-weight: 600;
+`;
+
+export const StyledBox = styled.div`
+  width: 100%;
+  min-height: 0;
+
+  position: absolute;
+  top: 90px;
+  left: 0;
+  right: 0;
+  bottom: 25px;
+
+  padding: 25px 25px;
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  & > .contentInput {
+    font-size: 20px;
+  }
 `;
 
 export const ReceiveUser = styled.div`
   display: flex;
-  margin: 10px 0 20px 0;
+  margin: 20px 0 30px 0;
 `;
 
 export const SendBox = styled.div`
   display: flex;
   align-items: center;
-  margin-top: 30px;
+  margin-top: 50px;
 `;
