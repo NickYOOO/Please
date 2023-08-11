adeca632e37c126c64d7312bc6099b002ea1fad2
Nick YOOO <outgrowthr@gmail.com>
2023-08-12 00:42:03 +0900

    Feat:사용자 상태 랜더링 기능구현 Feat:로컬스토리지 데이터 불러오기

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,47 @@
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import Logo from '../../assets/img/logo.svg';
 import * as Styled from './Header.styles';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
 
+interface User {
+  userName: string;
+}
 const Header = () => {
   const paths = ['/signup', '/login', '/', '/post', '/board', '/report'];
   const dynamicPaths = /^\/detail|user\/[\w\d]+$/;
+  const [cookies, setCookie, removeCookie] = useCookies();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const storedResponse = localStorage.getItem('response');
+    if (storedResponse) {
+      const parsedResponse = JSON.parse(storedResponse);
+      const username = parsedResponse.user.username;
+      setUser({ userName: username });
+      setIsLoggedIn(true);
+    }
+    console.log(storedResponse);
+  }, []);
 
   if (!paths.includes(window.location.pathname) && !dynamicPaths.test(window.location.pathname)) {
     return null;
   }
-
   const moveToMain = () => {
     window.location.href = '/';
   };
 
+  const handleLogout = async () => {
+    try {
+      localStorage.removeItem('response');
+      removeCookie('accessToken');
+      setIsLoggedIn(false);
+      alert('로그아웃 돼쓰 빠세!!');
+    } catch (error) {
+      console.error('로그아웃 오류:', error);
+    }
+  };
   return (
     <Styled.Header>
       <Styled.TitleBox onClick={moveToMain}>
@@ -21,10 +49,19 @@ const Header = () => {
         <h1>부탁해</h1>
       </Styled.TitleBox>
       <Styled.UserBox>
-        <>
-          <Link to={`/login`}>로그인</Link>
-          <Link to={`/signup`}>회원가입</Link>
-        </>
+        {isLoggedIn ? (
+          <>
+            <Link to={`/mypage`}>{user?.userName}님</Link>
+            <Link to="#" onClick={handleLogout}>
+              로그아웃
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to={`/login`}>로그인</Link>
+            <Link to={`/signup`}>회원가입</Link>
+          </>
+        )}
       </Styled.UserBox>
     </Styled.Header>
   );
