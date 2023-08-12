22c19f0b91d2155ad4411effb42390a0b0e7870e
Choi Yoonseo <choiys1103@naver.com>
2023-08-13 00:41:27 +0900

    feat: 상세페이지에서 쪽지 보내기, 글 작성 시 이메일, 유저네임 저장

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
+type TypeUserInput = React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>;
 
-
-function useInput(initialValue: string=""):[string,React.Dispatch<React.SetStateAction<string>>,(e:React.ChangeEvent<HTMLInputElement>) => void ] {
-  const [value, setValue] = useState<string>(initialValue)
-  const handler = (e:React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)
-  return [value,setValue, handler]
+function useInput(initialValue: string = ''): [string, (handler: TypeUserInput) => void] {
+  const [value, setValue] = useState<string>(initialValue);
+  const handler = (e: TypeUserInput) => setValue(e.target.value);
+  return [value, handler];
 }
 
-export default useInput
+export default useInput;
