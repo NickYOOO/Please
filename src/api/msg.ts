c8d4fa4b336621cbd35fe1ccc8430d94f577aab0
Choi Yoonseo <choiys1103@naver.com>
2023-08-12 16:15:36 +0900

    chore: 충돌 해결

--- src/api/msg.ts
+++ src/api/msg.ts
@@ -1,19 +1,13 @@
 import axios from 'axios';
-<<<<<<< HEAD
-
-export const getMsg = async () => {
-  const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/msg`);
-  return response.data;
-};
-=======
 import { IMsg } from '../components/types';
+
 const URL = process.env.REACT_APP_SERVER_URL;
 
-
-const sendMsg = async (msg:IMsg) => {
-  await axios.post(`${process.env.REACT_APP_SERVER_URL}/msg`, msg);
+export const getMsg = async () => {
+  const response = await axios.get(`${URL}/msg`);
+  return response.data;
 };
 
-
-export {sendMsg}
->>>>>>> 0d8fcb09341d782f0963cf95566eee8cc7182bd1
+export const sendMsg = async (msg: IMsg) => {
+  await axios.post(`${URL}/msg`, msg);
+};
