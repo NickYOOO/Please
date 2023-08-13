76b1f1053be34144ff21d112db64d6ec4fceb8ed
Choi Yoonseo <choiys1103@naver.com>
2023-08-13 21:52:16 +0900

    fix: notfound -> home 이동 시 푸터 안 보이는 오류 해결

--- src/components/notFound/NotFound.styles.ts
+++ src/components/notFound/NotFound.styles.ts
@@ -6,19 +6,20 @@ export const FullPageBox = styled.div`
   align-items: center;
   justify-content: center;
   height: 100vh;
-  margin-top: -40px;
+  margin-top: -50px;
   padding: 10px;
 
   & > h1 {
-    font-size: 60px;
+    font-size: 55px;
     color: #000000;
     margin: 40px 0 40px 0;
     word-break: keep-all;
   }
 
   & > p {
-    font-size: 34px;
+    font-size: 25px;
     font-weight: 500;
+    line-height: 1.5;
     color: #000000;
     word-break: keep-all;
   }
--- src/components/notFound/NotFound.tsx
+++ src/components/notFound/NotFound.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import * as Styled from './NotFound.styles';
 import { Button, Space } from 'antd';
 import Img404 from '../../assets/img/404.png';
-import { useNavigate } from 'react-router-dom';
 
 const NotFound: React.FC = () => {
-  const navigate = useNavigate();
-  const goBack = () => navigate('/');
+  const goBack = () => (window.location.href = '/');
 
   return (
     <Styled.FullPageBox>
       <img src={Img404} alt="404.img" />
-      <h1>페이지를 찾을 수 없습니다.</h1>
-      <p>페이지가 존재하지 않거나, 사용할 수 없는 페이지 입니다.</p>
-      <p>입력하신 주소가 정확한지 다시 한 번 확인해주세요.</p>
+      <h1>페이지를 찾을 수 없습니다</h1>
+      <p>페이지가 존재하지 않거나, 사용할 수 없는 페이지 입니다</p>
+      <p>입력하신 주소가 정확한지 다시 한 번 확인해주세요</p>
       <br />
       <br />
       <Space direction="vertical" style={{ width: '40%' }}>
