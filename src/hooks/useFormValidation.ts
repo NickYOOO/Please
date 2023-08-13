c853e2aa113e1a7538442031d545a3ef61838905
Nick YOOO <outgrowthr@gmail.com>
2023-08-13 17:17:35 +0900

    Chore: env 수정

--- src/hooks/useFormValidation.ts
+++ src/hooks/useFormValidation.ts
@@ -32,7 +32,7 @@ const useFormValidation = () => {
       const { email } = values;
 
       try {
-        const response = await axios.get(`http://localhost:3001/users?email=${email}`);
+        const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/users?email=${email}`);
         console.log(response.data);
         return response.data.length > 0;
       } catch (error) {
